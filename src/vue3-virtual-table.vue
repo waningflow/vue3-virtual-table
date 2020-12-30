dcce3a98a2d5efc510740bd0176b2f9d2d9ab67d
waningflow <jack1995woods@gmail.com>
2020-12-30 22:51:05 +0800

    feat: add dependency

--- src/vue3-virtual-table.vue
+++ src/vue3-virtual-table.vue
@@ -1,69 +1,18 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-interface SampleData {
-  counter: number;
-  initCounter: number;
-  message: {
-    action: string | null;
-    amount: number | null;
-  };
-}
-
-export default /*#__PURE__*/ defineComponent({
-  name: 'Vue3VirtualTable', // vue component name
-  data(): SampleData {
-    return {
-      counter: 5,
-      initCounter: 5,
-      message: {
-        action: null,
-        amount: null,
-      },
-    };
-  },
-  computed: {
-    changedBy() {
-      const { message } = this as SampleData;
-      if (!message.action) return 'initialized';
-      return `${message?.action} ${message.amount ?? ''}`.trim();
-    },
-  },
-  methods: {
-    increment(arg: Event | number): void {
-      const amount = typeof arg !== 'number' ? 1 : arg;
-      this.counter += amount;
-      this.message.action = 'incremented by';
-      this.message.amount = amount;
-    },
-    decrement(arg: Event | number): void {
-      const amount = typeof arg !== 'number' ? 1 : arg;
-      this.counter -= amount;
-      this.message.action = 'decremented by';
-      this.message.amount = amount;
-    },
-    reset(): void {
-      this.counter = this.initCounter;
-      this.message.action = 'reset';
-      this.message.amount = null;
-    },
-  },
-});
-</script>
-
 <template>
   <div class="vue3-virtual-table">
-    <p>
-      The counter was {{ changedBy }} to <b>{{ counter }}</b
-      >.
-    </p>
-    <button @click="increment">Click +1</button>
-    <button @click="decrement">Click -1</button>
-    <button @click="increment(5)">Click +5</button>
-    <button @click="decrement(5)">Click -5</button>
-    <button @click="reset">Reset</button>
+    <VirtualList :data="[]"></VirtualList>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { VirtualList } from 'vue3-virtual-list';
+
+export default /*#__PURE__*/ defineComponent({
+  name: 'Vue3VirtualTable',
+  components: { VirtualList },
+});
+</script>
 
 <style scoped>
 .vue3-virtual-table {
@@ -74,8 +23,5 @@ export default /*#__PURE__*/ defineComponent({
   background: #eaeaea;
   text-align: center;
   padding: 25px;
-}
-.vue3-virtual-table p {
-  margin: 0 0 1em;
 }
 </style>
